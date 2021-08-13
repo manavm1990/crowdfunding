a1cc6f34397a247688fffdcf90e9ca277569a980
Manav Misra <manavm@visionwebsoft.com>
2021-08-13 14:11:57 -0500

    :recycle: UserController static _stripUser

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,14 +1,28 @@
-import User from "../models/User.js";
+import models from "../models/index.js";
 
 class UserController {
+  static _stripUser(user) {
+    const { id, password, ...strippedUser } = user.dataValues;
+    return strippedUser;
+  }
+
+  static async create(newUser) {
+    const user = await models.User.create(newUser);
+    if (user) {
+      return UserController._stripUser(user);
+    }
+
+    throw new Error("Unable to create user!");
+  }
+
   static async login(email, password) {
-    const existingUser = await User.findOne({ where: { email } });
+    // `existingUser` will be an instance of
+    const existingUser = await models.User.findOne({ where: { email } });
 
     const validated = await existingUser?.checkPassword(password);
 
     if (validated) {
-      const { id, password, ...user } = existingUser.dataValues;
-      return user;
+      return UserController._stripUser(existingUser);
     }
 
     throw new Error("❗ Access Denied");
